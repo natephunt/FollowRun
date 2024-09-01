c84fbd3d7d9d88829990caeb65c1e867fab8fe29
Nathan Hunt <nathanhunt@nathans-mbp.lan>
2024-09-01 16:46:47 -0700

    made it use classes

--- main.py
+++ main.py
@@ -1,130 +1,157 @@
 import pygame
 import random
 import math
-from pygame.locals import*
+from pygame.locals import *
 import time
 
-# variables
-background = (250,250,250)
-red = (250, 10, 10)
-blue = (10, 10, 250)
-running = True
+class Game:
+    width, height = 640, 480
+    background = (250, 250, 250)
+    running = True
 
-# agent variables
-left, right, up, down = False, False, False, False
-px, py = 100, 100
-pSpeed = 6
+    def __init__(self):
+        pygame.init()
+        width, height = 640, 480
+        self.screen = pygame.display.set_mode((width, height))
+        pygame.display.set_caption('FollowRun')
+        self.background = (250, 250, 250)
+        self.running = True
+        self.reset()
 
-# follower variables
-fx, fy = 300, 100
-fvx, fvy = 2, 2
-fSpeed = 4
+    def reset(self):
+        pass
 
-# make screen
-pygame.init()
-width, height = 640, 480
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption('FollowRun')
-screen.fill(background)
+    def pollInputs(self, agent):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    agent.left = True
+                if event.key == pygame.K_RIGHT:
+                    agent.right = True
+                if event.key == pygame.K_UP:
+                    agent.up = True
+                if event.key == pygame.K_DOWN:
+                    agent.down = True
+                if event.key == pygame.K_q:
+                    self.running = False
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT:
+                    agent.left = False
+                if event.key == pygame.K_RIGHT:
+                    agent.right = False
+                if event.key == pygame.K_UP:
+                    agent.up = False
+                if event.key == pygame.K_DOWN:
+                    agent.down = False
 
-# functions
-def draw():
-    #fill over last frame
-    screen.fill(background)
+    def update(self):
+        agent.update()
+        follower.update()
 
-    #draw the player
-    pygame.draw.circle(screen, blue, (px,py), 20)
-    pygame.draw.circle(screen, red, (fx,fy), 20)
+    def draw(self):
+        self.screen.fill(self.background)
+        agent.draw()
+        follower.draw()
+        pygame.display.flip()
 
-    #display
-    pygame.display.flip()
+    def get_distance(self):
+        return math.sqrt((agent.x - follower.x) ** 2 + (agent.y - follower.y) ** 2)
 
-def inputs():
-    global left, right, up, down, running
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                left = True
-            if event.key == pygame.K_RIGHT:
-                right = True
-            if event.key == pygame.K_UP:
-                up = True
-            if event.key == pygame.K_DOWN:
-                down = True
-            if event.key == pygame.K_q:
-                running = False
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT:
-                left = False
-            if event.key == pygame.K_RIGHT:
-                right = False
-            if event.key == pygame.K_UP:
-                up = False
-            if event.key == pygame.K_DOWN:
-                down = False
+    def loop(self):
+        while self.running:
+            self.pollInputs(agent)
+            self.update()
+            self.draw()
+            time.sleep(0.016)  # ~60fps
 
-def update():
-    #handle agent stuff
-    global px,py
-    if left:
-        px-=pSpeed
-    if right:
-        px+=pSpeed
-    if up:
-        py-=pSpeed
-    if down:
-        py+=pSpeed
+class Player:
+    x, y, vx, vy, drag = 0, 0, 0, 0, 0
+    color = (20, 20, 20)
 
-    #handle follower stuff
-    global fx, fy, fvx, fvy
-    fvx *= 0.975
-    fvy *= 0.975
-    fx += fvx
-    fy += fvy
-    if fx < px:
-        fvx += 0.3
-    if fx > px:
-        fvx -= 0.3
-    if fy < py:
-        fvy += 0.3
-    if fy > py:
-        fvy -= 0.3
-    if fx<0:
-        fvx *=-1
-        fx+=1
-    if fx>width:
-        fvx *=-1
-        fx-=1
-    if fy<0:
-        fvy *=-1
-        fy+=1
-    if fy>height:
-        fvy *=-1
-        fy-=1
-    if(random.random()>0.97):
-        fvx += random.random()*7
-    if(random.random()>0.97):
-        fvx -= random.random()*7
-    if(random.random()>0.97):
-        fvy += random.random()*7
-    if(random.random()>0.97):
-        fvy -= random.random()*7
+    def __init__(self, x, y, drag, color):
+        self.x = x
+        self.y = y
+        self.drag = drag
+        self.color = color
+    
+    def update(self):
+        self.vx *= self.drag
+        self.vy *= self.drag
+        self.x += self.vx
+        self.y += self.vy
 
-def getDist(): # returns a double value of the distance between dots
-    return math.sqrt((px-fx)(px-fx) + (py-fy)(py-fy))
+        if self.x < 0:
+            self.vx *= -1
+            self.x += 1
+        if self.x > game.width:
+            self.vx *= -1
+            self.x -= 1
+        if self.y < 0:
+            self.vy *= -1
+            self.y += 1
+        if self.y > game.height:
+            self.y *= -1
+            self.y -= 1
+    
+    def draw(self):
+        pygame.draw.circle(game.screen, self.color, (self.x, self.y), 20)
 
-# main game loop
-def loop():
-    while running:
-        inputs()
-        update()
+class Agent(Player):
+    speed = 6
+    left = False
+    right = False
+    up = False
+    down = False
 
-        draw()
-        time.sleep(0.016) # ~60fps
-        
+    def __init__(self, x, y, drag, color):
+        super().__init__(x, y, drag, color)
 
-# run
+    def update(self):
+        self.vx = 0
+        self.vy = 0
+
+        if self.left:
+            self.vx = -self.speed
+        if self.right:
+            self.vx = self.speed
+        if self.up:
+            self.vy = -self.speed
+        if self.down:
+            self.vy = self.speed
+
+        super().update()
+
+class Follower(Player):
+    def __init__(self, x, y, drag, color):
+        super().__init__(x, y, drag, color)
+
+    def update(self):
+        #home follower
+        if self.x < agent.x:
+            self.vx += 0.3
+        if self.x > agent.x:
+            self.vx -= 0.3
+        if self.y < agent.y:
+            self.vy += 0.3
+        if self.y > agent.y:
+            self.vy -= 0.3
+
+        # Random velocity changes for the follower
+        if random.random() > 0.97:
+            self.vx += random.random() * 7
+        if random.random() > 0.97:
+            self.vx -= random.random() * 7
+        if random.random() > 0.97:
+            self.vy += random.random() * 7
+        if random.random() > 0.97:
+            self.vy -= random.random() * 7
+
+        super().update()
+
 if __name__ == "__main__":
-    loop()
+    game = Game()
+    agent = Agent(300, 200, 1, (10, 10, 250))
+    follower = Follower(50, 50, 0.975, (250, 10, 10))
+    game.loop()
